36781534f53815ad6f09aaa9ecd457d5fc58f82c
Radia4 <113570901+Radia4@users.noreply.github.com>
2023-03-18 19:07:41 -0600

    Lab 9 Completed

--- src/java/models/User.java
+++ src/java/models/User.java
@@ -6,81 +6,128 @@
 package models;
 
 import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
  * @author radia
  */
+@Entity
+@Table(name = "user")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "User_1.findAll", query = "SELECT u FROM User_1 u")
+    , @NamedQuery(name = "User_1.findByEmail", query = "SELECT u FROM User_1 u WHERE u.email = :email")
+    , @NamedQuery(name = "User_1.findByFirstName", query = "SELECT u FROM User_1 u WHERE u.firstName = :firstName")
+    , @NamedQuery(name = "User_1.findByLastName", query = "SELECT u FROM User_1 u WHERE u.lastName = :lastName")
+    , @NamedQuery(name = "User_1.findByPassword", query = "SELECT u FROM User_1 u WHERE u.password = :password")})
 public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "email")
     private String email;
-    private int active;
+    @Basic(optional = false)
+    @Column(name = "first_name")
     private String firstName;
+    @Basic(optional = false)
+    @Column(name = "last_name")
     private String lastName;
+    @Basic(optional = false)
+    @Column(name = "password")
     private String password;
-    private int role;
+    @JoinColumn(name = "role", referencedColumnName = "role_id")
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    private Role role;
 
     public User() {
     }
 
-    public User(String email, int active, String firstName, String lastName, String password, int role) {
+    public User(String email) {
         this.email = email;
-        this.active = active;
+    }
+
+    public User(String email, String firstName, String lastName, String password) {
+        this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
         this.password = password;
-        this.role = role;
     }
 
     public String getEmail() {
         return email;
-    }
-    
-    public int getActive() {
-        return active;
-    }
-    
-    public String getFirstName() {
-        return firstName;
-    }
-    
-    public String getLastName() {
-        return lastName;
-    }
-    
-    public String getPassword() {
-        return password;
-    }
-    
-     public int getRole() {
-        return role;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public void setActive(int active) {
-        this.active = active;
-    }
-    
-    public void setActive(boolean active) {
-        this.active = active ? 1 : 0;
+    public String getFirstName() {
+        return firstName;
     }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    public String getLastName() {
+        return lastName;
+    }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    
+
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public void setRole(int role) {
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
         this.role = role;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (email != null ? email.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.email == null && other.email != null) || (this.email != null && !this.email.equals(other.email))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "models.User_1[ email=" + email + " ]";
     }
     
 }
